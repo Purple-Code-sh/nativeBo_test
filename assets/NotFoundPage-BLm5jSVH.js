faf76d294e6c5b0624c915b839194cffc4e39b75
Purple-Code-sh <Purple-Code-sh@users.noreply.github.com>
2025-01-19 23:15:20 +0000

    deploy: caca15223878818bbeaad98f8658bee059aaeff1

--- assets/NotFoundPage-BLm5jSVH.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CC47OuoC.js";function s(){return e.jsx("div",{className:" bg-yellow-400  w-screen",children:"This is the 404 error page"})}export{s as default};
---
+++ assets/NotFoundPage-BssQ_ABP.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-B90e5rjT.js";function s(){return e.jsx("div",{className:" bg-yellow-400  w-screen",children:"This is the 404 error page"})}export{s as default};
