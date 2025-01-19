26c80abbb0c779f24163806bbb0110b310fefb1c
Purple-Code-sh <Purple-Code-sh@users.noreply.github.com>
2025-01-19 12:21:31 +0000

    deploy: 16d9976d1718d52a1490bc3059de8bb00529eeb4

--- assets/FavoritesPage-BuQsQvga.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-Dhaq65pZ.js";function r(){return e.jsx("div",{className:" bg-red-400 w-screen",children:"This is favorites page"})}export{r as default};
---
+++ assets/FavoritesPage-DFZSS4Lr.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C7fcA52M.js";function r(){return e.jsx("div",{className:" bg-red-400 w-screen",children:"This is favorites page"})}export{r as default};
